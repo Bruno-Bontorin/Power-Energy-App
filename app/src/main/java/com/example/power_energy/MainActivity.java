eb9501a611d87f2d302ecede7bcbe2b44cac2d57
Bruno Stival Bontorin <bruno.bontorin.001@outlook.com>
2021-11-07 23:16:35 -0300

    Changing Language and Updating Functions

--- app/src/main/java/com/example/power_energy/MainActivity.java
+++ app/src/main/java/com/example/power_energy/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.power_energy;
 
-
+import android.content.res.Resources;
 import android.os.Bundle;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import android.text.TextUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import android.widget.Button;
@@ -29,55 +22,67 @@ import android.view.View;
 import com.example.power_energy.ui.main.SectionsPagerAdapter;
 import com.example.power_energy.databinding.ActivityMainBinding;
 
+/* Imports Unused */
+/*
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+*/
 
-public class MainActivity<calc> extends AppCompatActivity {
-  Button btn_calculo;
+public class MainActivity extends AppCompatActivity {
+  Button btn_calculation;
 
   private ActivityMainBinding binding;
+  public String[] gadgetsArray;
+  public String[] voltageArray;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    /* Binding */
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
+
+    /* Get String.xml Resources */
+    Resources resources = getResources();
+
+    /* Set arrays of resources  */
+    gadgetsArray = resources.getStringArray(R.array.gadgets_name_array);
+    voltageArray = resources.getStringArray(R.array.voltage_array);
 
     SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
     ViewPager viewPager = binding.viewPager;
     viewPager.setAdapter(sectionsPagerAdapter);
     TabLayout tabs = binding.tabs;
     tabs.setupWithViewPager(viewPager);
-    FloatingActionButton fab = binding.fab;
 
-    fab.setOnClickListener(new View.OnClickListener() {
+    /* Function fab icon */
+    /* FloatingActionButton fab = binding.fab; */
+    /*    fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             .setAction("Action", null).show();
       }
-    });
-
+    });*/
 
     // Spinner element
-    Spinner spinner = (Spinner) findViewById(R.id.voltagem);
-
-    // Spinner Drop down elements
-    String voltagemArray[] = {"127", "220"};
+    Spinner spinner = findViewById(R.id.voltage);
 
     // Creating adapter for spinner
-    ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, voltagemArray);
+    ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, voltageArray);
 
     // Drop down layout style - list view with radio button
     dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-    // attaching data adapter to spinner
+    // Attaching data adapter to spinner
     spinner.setAdapter(dataAdapter);
 
     // Spinner element
-    Spinner aparelho = (Spinner) findViewById(R.id.aparelhos);
+    Spinner gadget = findViewById(R.id.gadgets);
 
-    aparelho.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+    gadget.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
       @Override
       public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
         refresh();
@@ -85,26 +90,25 @@ public class MainActivity<calc> extends AppCompatActivity {
 
       @Override
       public void onNothingSelected(AdapterView<?> parentView) {
+        System.out.println("\n\n Nothing Selected -> Function: onNothingSelected \n\n");
+        Toast.makeText(getApplicationContext(), "\n\n Nothing Selected -> Function: onNothingSelected \n\n", Toast.LENGTH_LONG).show();
       }
 
     });
 
-    // Spinner Drop down elements
-    String aparelhosArray[] = {"Personalizado", "Chuveiro", "Geladeira", "Televisão", "Lâmpada"};
-
     // Creating adapter for spinner
-    ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, aparelhosArray);
+    ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, gadgetsArray);
 
     // Drop down layout style - list view with radio button
     dataAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
     // attaching data adapter to spinner
-    aparelho.setAdapter(dataAdapter1);
+    gadget.setAdapter(dataAdapter1);
     onClickButton();
   }
 
   public void getValueSpinnerVoltage() {
-    Spinner voltage = (Spinner) findViewById(R.id.voltagem);
+    Spinner voltage = findViewById(R.id.voltage);
     String Text = voltage.getSelectedItem().toString();
     Toast.makeText(getApplicationContext(), Text, Toast.LENGTH_LONG).show();
   }
@@ -126,142 +130,131 @@ public class MainActivity<calc> extends AppCompatActivity {
     int editTextToInt = 0;
     switch (verifyFields(editText)) {
       case 1:
-        editText.setError("Preenchimento Obrigatório!!");
-        System.out.println("Preenchimento Obrigatório!!");
+        editText.setError("Required");
+        System.out.println("Required!");
         editTextToInt = 0;
         break;
       case 2:
-        editText.setError("Informação incorreta!!");
-        System.out.println("Informação incorreta!!");
+        editText.setError("Incorrect Information");
+        System.out.println("Incorrect Information");
         editTextToInt = 0;
         break;
       case 3:
-        //editText.setError("Retorno da função!!");
-        System.out.println("TESTE FUNÇÃO!!!! ->>" + TextUtils.isEmpty(editTextToString));
-        System.out.println("NÃO VAZIO!!!! ->>" + editTextToString);
+        System.out.println("\n\nReturn function: verifyFields -> " + TextUtils.isEmpty(editTextToString) + "\n\n");
+        System.out.println("\n\nNot Empty -> " + editTextToString + "\n\n");
         editTextToInt = Integer.parseInt(editTextToString);
         break;
       default:
-        System.out.println("Retorno anormal - consulte ajuda");
+        System.out.println("\n\n Abnormal return - see help... -> Default break function: editTextToInt \n\n");
         break;
-    }
-
-    if (verifyFields(editText) == 1) {
-
-    } else if (verifyFields(editText) == 2) {
-
-    } else if (verifyFields(editText) == 3) {
-
     }
     return editTextToInt;
   }
 
-  public void setValueByObj(EditText potencia, EditText tempo, EditText quantidade, EditText amperagem) {
-    Spinner voltage = (Spinner) findViewById(R.id.voltagem);
-    Spinner gadgets = (Spinner) findViewById(R.id.aparelhos);
+  public void setValueByObj(EditText time, EditText amperage, EditText amount, EditText potency) {
+    /* Get String.xml Resources */
+    Resources resources = getResources();
+
+    /* Create Spinner */
+    Spinner voltage = findViewById(R.id.voltage);
+    Spinner gadgets = findViewById(R.id.gadgets);
     String gadgetsValue = gadgets.getSelectedItem().toString();
+
     switch (gadgetsValue) {
-      case "Personalizado":
-        potencia.setText("");
-        tempo.setText("");
-        quantidade.setText("");
-        amperagem.setText("");
+      case "Custom":
+        /* Set value array of resources  */
+        String[] customObjArray = resources.getStringArray(R.array.custom_obj_array);
 
-        System.out.println("");
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\nEDIT TEXT VALUE: " + potencia.getText().toString() + "\n");
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\nEDIT TEXT VALUE: " + tempo.getText().toString() + "\n");
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\nEDIT TEXT VALUE: " + quantidade.getText().toString() + "\n");
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\nEDIT TEXT VALUE: " + amperagem.getText().toString() + "\n");
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\nEDIT TEXT VALUE: " + voltage.getSelectedItem().toString() + "\n\n");
-
+        /* Set values through Array */
+        voltage.setSelection(Integer.parseInt(customObjArray[0]));
+        time.setText(customObjArray[1]);
+        amperage.setText(customObjArray[2]);
+        amount.setText(customObjArray[3]);
+        potency.setText(customObjArray[4]);
         break;
-      case "Chuveiro":
-        // Difinindo valores fixos
-        potencia.setText("2700");
-        tempo.setText("0.25");
-        quantidade.setText("1");
-        amperagem.setText("12");
+      case "Shower":
+        /* Set value array of resources  */
+        String[] showerObjArray = resources.getStringArray(R.array.shower_obj_array);
 
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\n\nEDIT TEXT VALUE: " + potencia.getText().toString() + "\n\n");
+        /* Set values through Array */
+        voltage.setSelection(Integer.parseInt(showerObjArray[0]));
+        time.setText(showerObjArray[1]);
+        amperage.setText(showerObjArray[2]);
+        amount.setText(showerObjArray[3]);
+        potency.setText(showerObjArray[4]);
         break;
-      case "Geladeira":
-        potencia.setText("2200");
-        tempo.setText("24");
-        quantidade.setText("1");
-        amperagem.setText("10");
-        voltage.setSelection(1);
+      case "Refrigerator":
+        /* Set value array of resources  */
+        String[] refrigeratorObjArray = resources.getStringArray(R.array.refrigerator_obj_array);
 
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\n\nEDIT TEXT VALUE: " + potencia.getText().toString() + "\n\n");
+        /* Set values through Array */
+        voltage.setSelection(Integer.parseInt(refrigeratorObjArray[0]));
+        time.setText(refrigeratorObjArray[1]);
+        amperage.setText(refrigeratorObjArray[2]);
+        amount.setText(refrigeratorObjArray[3]);
+        potency.setText(refrigeratorObjArray[4]);
         break;
-      case "Televisão":
-        potencia.setText("");
-        tempo.setText("");
-        quantidade.setText("");
-        amperagem.setText("");
+      case "Television":
+        /* Set value array of resources  */
+        String[] televisionObjArray = resources.getStringArray(R.array.television_obj_array);
 
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\n\nEDIT TEXT VALUE: " + potencia.getText().toString() + "\n\n");
+        /* Set values through Array */
+        voltage.setSelection(Integer.parseInt(televisionObjArray[0]));
+        time.setText(televisionObjArray[1]);
+        amperage.setText(televisionObjArray[2]);
+        amount.setText(televisionObjArray[3]);
+        potency.setText(televisionObjArray[4]);
         break;
-      case "Lâmpada":
-        potencia.setText("");
-        tempo.setText("");
-        quantidade.setText("");
-        amperagem.setText("");
+      case "Lamp":
+        /* Set value array of resources  */
+        String[] lampObjArray = resources.getStringArray(R.array.lamp_obj_array);
 
-        System.out.println("\n\nGADGETS VALUE: " + gadgetsValue + "\n\nEDIT TEXT VALUE: " + potencia.getText().toString() + "\n\n");
+        /* Set values through Array */
+        voltage.setSelection(Integer.parseInt(lampObjArray[0]));
+        time.setText(lampObjArray[1]);
+        amperage.setText(lampObjArray[2]);
+        amount.setText(lampObjArray[3]);
+        potency.setText(lampObjArray[4]);
         break;
       default:
-        System.out.println("NÃO VAZIO!!!! ->>" + gadgetsValue);
+        System.out.println("\n\n Nothing to execute... -> Default break funtion: setValueByObj \n\n");
         break;
     }
   }
 
   public void refresh() {
-    final EditText potential = (EditText) findViewById(R.id.potencia);
-    final EditText tempo = (EditText) findViewById(R.id.tempo);
-    final EditText qtde = (EditText) findViewById(R.id.qtde);
-    final EditText ampe = (EditText) findViewById(R.id.amperagem);
-    Spinner spinner = (Spinner) findViewById(R.id.voltagem);
-    setValueByObj(potential, tempo, qtde, ampe);
+    /* Get String.xml Resources */
+    Resources resources = getResources();
+
+    /* Set value array of resources  */
+    gadgetsArray = resources.getStringArray(R.array.gadgets_name_array);
+
+    /* Set edit text value to text */
+    final EditText potency = findViewById(R.id.potency);
+    final EditText time = findViewById(R.id.time);
+    final EditText amount = findViewById(R.id.amount);
+    final EditText amperage = findViewById(R.id.amperage);
+
+    /* Performing function */
+    setValueByObj(time, amperage, amount, potency);
   }
 
   public void onClickButton() {
 
-    final EditText potential = (EditText) findViewById(R.id.potencia);
-    final EditText tempo = (EditText) findViewById(R.id.tempo);
-    final EditText qtde = (EditText) findViewById(R.id.qtde);
-    final EditText ampe = (EditText) findViewById(R.id.amperagem);
-    Spinner spinner = (Spinner) findViewById(R.id.voltagem);
+    /* Set edit text value to text */
+    final EditText potency = findViewById(R.id.potency);
+/*    final EditText time = findViewById(R.id.time);
+    final EditText amount = findViewById(R.id.amount);
+    final EditText amperage = findViewById(R.id.amperage);*/
 
 
-    btn_calculo = (Button) findViewById(R.id.btn_calculo);
+    btn_calculation = findViewById(R.id.btn_calculation);
 
     //editTextToInt(potential);
-    btn_calculo.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        getValueSpinnerVoltage();
-        int varPot = editTextToInt(potential);
-        System.out.println("NÃO VAZIO!!!! ->>" + varPot);
-        /*if(varTempo == 0) {
-        nao foi possivv....
-        } else {
-          if (varQtde == 0)
-            nao foi possivv....
-        } else {
-          if (varPot == 0) {
-            if(varAmp == 0) {
-              nao foi possivv...
-            } else {
-              função(varPot);
-              cal()
-            }
-          } else  {
-            function(varApm);
-            cal()
-          }
-        }*/
-
-      }
+    btn_calculation.setOnClickListener(view -> {
+      getValueSpinnerVoltage();
+      int varPot = editTextToInt(potency);
+      System.out.println("\n\n Not Empty... -> " + varPot + " \n\n");
     });
   }
 }
